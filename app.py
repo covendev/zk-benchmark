db2efc4d5b34791a9d98afe3a62bbe6fecfab63d
covendev <namita1698@gmail.com>
2024-05-09 00:19:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import pandas as pd
 import matplotlib.pyplot as plt
-from io import BytesIO
+from io import StringIO
 import base64
 from flask_frozen import Freezer
 app = Flask(__name__)
